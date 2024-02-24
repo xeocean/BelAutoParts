90f120d1c7cfb914b3b062c24b61386c4e23fd1a
Workbook80 <workbook1985@gmail.com>
2024-02-24 15:38:18 +0300

    v5.0

--- BelAutoParts/settings.py
+++ BelAutoParts/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'AppParts', 'static')
 
 # ONLY DEVELOPMENT
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'AppParts', 'media')
 
 # Default primary key field type
